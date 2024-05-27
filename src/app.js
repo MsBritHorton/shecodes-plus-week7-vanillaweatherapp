a51a2abcbe73fb6fb7238d42125e2a115d863143
MsBritHorton <166248852+MsBritHorton@users.noreply.github.com>
2024-05-26 20:11:15 -0500

    Updated Weather and City Refreshing
    Updated the .js to add new functions for refreshing the weather and searching for a city. Remaining issue is the CSS styling with the weather, icon, and unit showing.

--- src/app.js
+++ src/app.js
@@ -50,11 +50,92 @@ Humidity: <strong>87%</strong>, Wind: <strong>7.2km/h</strong></p>`;
 //When searching for a city (i.e. Paris), display the city
 //name on the page after the user submits the form.
 
+function refreshWeather(response){
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+let cityElement = document.querySelector("#current-city");
+cityElement.innerHTML = response.data.city;
+temperatureElement.innerHTML = Math.round(temperature);
+}
+
+
+
+function searchCity(city) {
+    let apiKey = "bca5e052413bdbcf0bf4oa7bbat0c995";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(refreshWeather);
+}
+
 function changeCityData(event) {
   event.preventDefault();
   let searchInputName = document.querySelector("#search-city-input");
   let currentCityName = document.querySelector("#current-city");
   currentCityName.innerHTML = searchInputName.value;
+  searchCity(searchInputName.value);
 }
 let citySearchForm = document.querySelector("#city-search-form");
-citySearchForm.addEventListener("input", changeCityData);
+citySearchForm.addEventListener("submit", changeCityData);
+
+searchCity("Chicago");
+
+
+
+
+
+
+
+
+/*function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = searchInputElement.value;
+  let units = "imperial";
+  let apiKey = "bca5e052413bdbcf0bf4oa7bbat0c995";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${searchInputElement.value}&key=bca5e052413bdbcf0bf4oa7bbat0c995&units=${units}`;
+  axios.get(apiURL).then(showTemperature);
+}
+
+//New code for API
+function showTemperature(response) {
+  let temperature = Math.round(response.data.temperature.current);
+  let currentTemperature = document.querySelector("#current-temperature-value");
+  currentTemperature.innerHTML = `${response.data.temperature.current}`;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+let searchForm = document.querySelector("#city-search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDateELement = document.querySelector("#current-details");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
+*/
