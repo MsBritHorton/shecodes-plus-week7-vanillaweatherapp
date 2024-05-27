2fdb28cb980a821e9f1e9a283c377f9085b98b54
MsBritHorton <166248852+MsBritHorton@users.noreply.github.com>
2024-05-26 20:52:45 -0500

    Updated wind speed API

--- src/app.js
+++ src/app.js
@@ -1,18 +1,21 @@
-
-
 function refreshWeather(response){
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
 let cityElement = document.querySelector("#current-city");
 let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windSpeedElement = document.querySelector("#wind-speed"); 
+
 
 
 
 cityElement.innerHTML = response.data.city;
 temperatureElement.innerHTML = Math.round(temperature);
 descriptionElement.innerHTML = response.data.condition.description;
-}
 
+humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+}
 
 
 function searchCity(city) {
@@ -83,7 +86,7 @@ let minute = currentTime.getMinutes();
 
 let currentDayInfo = document.querySelector(".current-details");
 currentDayInfo.innerHTML = `<p>${day} ${hour}:${minute}, ${descriptionElement} <br />
-Humidity: <strong>87%</strong>, Wind: <strong>7.2km/h</strong></p>`;
+Humidity: <strong>${humidityElement}%</strong>, Wind: <strong>7.2km/h</strong></p>`;
 
 
 
