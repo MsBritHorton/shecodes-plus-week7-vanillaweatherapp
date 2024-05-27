f1911e640a0f6c6d807aa4ee68905a43b3f8c638
MsBritHorton <166248852+MsBritHorton@users.noreply.github.com>
2024-05-26 21:22:12 -0500

    Updated weather icons to change with each country/city

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
 function refreshWeather(response){
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
-let cityElement = document.querySelector("#current-city");
-let descriptionElement = document.querySelector("#description");
-let humidityElement = document.querySelector("#humidity");
-let windSpeedElement = document.querySelector("#wind-speed"); 
+  let cityElement = document.querySelector("#current-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let iconElement = document.querySelector("#icon");
 
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+  descriptionElement.innerHTML = response.data.condition.description;
 
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-icon"/>`;
 
-
-cityElement.innerHTML = response.data.city;
-temperatureElement.innerHTML = Math.round(temperature);
-descriptionElement.innerHTML = response.data.condition.description;
-
-humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 }
 
 
